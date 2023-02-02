783c2f26a8f083550b4d1c0f98c3a48b79b3d1ee
汪浩 <wanghao@zonesion.comc.cn>
2023-02-02 13:00:32 +0800

    修复cvwin的 RuntimeError: dictionary changed size during iteration

--- marm_visual_control/script/cvwin.py
+++ marm_visual_control/script/cvwin.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import threading
-import cv2 as cv
+import cv2
 import sys
 import time
 import copy
@@ -10,6 +10,7 @@ from collections import OrderedDict
 this = sys.modules[__name__]
 this.showwins={}            #{"name":{"img":img,"update":True},}
 this.destroywins=[]
+this.resizewins=[]          #[  [] ]
 this.namedWindows={}        #{"name":True}
 this.createTrackbars=OrderedDict()     #{"winname":{"trackbarname":{"param":[trackbarname,winname,value,count,TrackbarCallback],"update":True,"value":0},},}
 this.createTrackbars_win=OrderedDict() #{"winname":createTrackbars_win}
@@ -40,54 +41,71 @@ def imshow(name,img):
 def destroyWindow(name):
     this.destroywins.append(name)
 
+def resizeWindow(name,x,y):
+    this.resizewins.append([name,x,y])
 
+this.st=time.time()
 def cvwin():
     while True:
-        for i in this.namedWindows.keys():
+        for i in list(this.namedWindows.keys()):
             if this.namedWindows[i]==True:
-                cv.namedWindow(i)
+                # cv2.namedWindow(i)
+                cv2.namedWindow(i,cv2.WINDOW_NORMAL)
                 this.namedWindows[i]=False
         
-        for m in this.createTrackbars.keys():   #win
+        for m in list(this.createTrackbars.keys()):   #win
             if this.namedWindows[m]==False:    #check win is open
                 for n in this.createTrackbars[m].keys(): #Trackbar
                     try :
-                        t=cv.getWindowProperty(m,cv.WND_PROP_VISIBLE)
+                        t=cv2.getWindowProperty(m,cv2.WND_PROP_VISIBLE)
                     except:
                         pass
                     if this.createTrackbars[m][n]["update"]==True and t and this.namedWindows[m]==False:   #检查窗口打开:
                         data=this.createTrackbars[m][n]["param"]
-                        cv.createTrackbar(data[0],data[1],data[2],data[3],data[4])
+                        cv2.createTrackbar(data[0],data[1],data[2],data[3],data[4])
                         this.createTrackbars[m][n]["update"]=False
 
-        for i in this.showwins.keys():
+        if len(this.resizewins)>0:
+            rswin=this.resizewins[0]
+            try :
+                if cv2.getWindowProperty(rswin[0],cv2.WND_PROP_VISIBLE) and this.namedWindows[rswin[0]]==False:
+                    del this.resizewins[0]
+                    cv2.resizeWindow(rswin[0],rswin[1],rswin[2])
+            except:
+                pass
+            
+        for i in list(this.showwins.keys()):
             win=this.showwins[i]
             if win["update"]==True:
-                cv.imshow(i, win["img"])
+                cv2.imshow(i, win["img"])
                 win["update"]=False
 
-        while len(this.destroywins)>0:
+        if len(this.destroywins)>0:
             try:
                 delwin=this.destroywins[0]
-                if cv.getWindowProperty(delwin,cv.WND_PROP_VISIBLE):
+                if cv2.getWindowProperty(delwin,cv2.WND_PROP_VISIBLE):
                     del this.destroywins[0]
                     del this.showwins[delwin]
-                    cv.destroyWindow(delwin)
-            finally :
-                break  
+                    cv2.destroyWindow(delwin)
+            except Exception as e:
+                print(e)
+                print("destroyWindow error! retry")
+                pass  
                 
-        for m in this.createTrackbars.keys():   #win
+        for m in list(this.createTrackbars.keys()):   #win
             try :
-                if cv.getWindowProperty(m,cv.WND_PROP_VISIBLE):   #检查窗口打开
+                if cv2.getWindowProperty(m,cv2.WND_PROP_VISIBLE):   #检查窗口打开
                     for n in this.createTrackbars[m].keys():
                         if this.createTrackbars[m][n]["param"]:
                             data=this.createTrackbars[m][n]["param"]
-                            this.createTrackbars[m][n]["value"]=cv.getTrackbarPos(data[0],data[1])
+                            this.createTrackbars[m][n]["value"]=cv2.getTrackbarPos(data[0],data[1])
                             # print(m,n,this.createTrackbars[m][n]["value"])
             except:
                 break
-
-        cv.waitKey(10)
+        if time.time()-this.st>30:
+            print("Win Thread Running")
+            this.st=time.time()
+        cv2.waitKey(10)
 
 t = threading.Thread(target=cvwin)
 t.setDaemon(True)
