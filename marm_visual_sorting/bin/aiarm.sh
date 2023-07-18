d69ea3a1223f9608b9c57790ea41b1de21099a15
汪浩 <wanghao@zonesion.comc.cn>
2023-07-18 14:28:07 +0800

    gx

--- marm_visual_sorting/bin/aiarm.sh
+++ marm_visual_sorting/bin/aiarm.sh
@@ -5,9 +5,9 @@ sudo chmod 666 /dev/ttyUSB*
 source /opt/ros/kinetic/setup.bash
 source ~/catkin_ws/devel/setup.bash
 
-cd /home/zonesion/catkin_ws/src/marm_visual_control/script
+cd /home/zonesion/catkin_ws/src/marm_visual_sorting/script
 
-roslaunch marm_visual_control aiarm-controller.launch &
+roslaunch marm_visual_sorting aiarm-controller.launch &
 sleep 26
 python main.py
 sleep 99999
